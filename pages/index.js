57ac3ab1294af82226d4ab9d36132798bbe3c382
sahib <sahib.alejandro@gmail.com>
2023-03-12 14:13:56 -0600

    Pre-rendering and Data Fetching

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,20 @@
 import Head from 'next/head';
 
-import Layout, { siteTitle } from '../components/layout';
+import { getSortedPostData } from '../lib/posts';
 import utilStyles from '../styles/utils.module.css';
+import Layout, { siteTitle } from '../components/layout';
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostData();
+
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -16,6 +27,21 @@ export default function Home() {
           (This is a sample website - you'll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+      </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1Px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
